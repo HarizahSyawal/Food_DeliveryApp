d934f2b80fc688f1bcb0896d75f462f88b834824
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-05-23 00:27:52 +0700

    Update slicing
    Home UI

--- lib/module/home/view/home_view.dart
+++ lib/module/home/view/home_view.dart
@@ -1,4 +1,7 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../../shared/widget/form/category_picker/category_picker.dart';
 import '../controller/home_controller.dart';
 
 class HomeView extends StatefulWidget {
@@ -8,8 +11,11 @@ class HomeView extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 249, 249, 249),
       appBar: AppBar(
+          elevation: 0,
           backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
           title: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -17,6 +23,7 @@ class HomeView extends StatefulWidget {
                 const Text(
                   "Current location",
                   style: TextStyle(
+                    color: Color.fromARGB(255, 168, 168, 168),
                     fontSize: 10,
                   ),
                 ),
@@ -33,6 +40,7 @@ class HomeView extends StatefulWidget {
                     Text(
                       "Makassar, Sulawesi Selatan",
                       style: TextStyle(
+                        color: Colors.black,
                         fontSize: 14.0,
                         fontWeight: FontWeight.bold,
                       ),
@@ -51,7 +59,10 @@ class HomeView extends StatefulWidget {
                       color: Colors.white,
                     ),
                   ),
-                  child: Icon(Icons.notifications),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
@@ -60,11 +71,11 @@ class HomeView extends StatefulWidget {
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              accountName: const Text("Andrew Garfield"),
-              accountEmail: const Text("capek@ngoding.com"),
+              accountName: const Text("Harizah Syawal"),
+              accountEmail: const Text("harisa1701@gmail.com"),
               currentAccountPicture: const CircleAvatar(
                 backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),
+                    "https://lh3.googleusercontent.com/a/AGNmyxYh6vcpGJ6e0YgFlRsqPDTScnac3Zv9HXFfHmmmCA=s576-c-no"),
               ),
               decoration: BoxDecoration(
                 color: Colors.blueGrey[900],
@@ -110,6 +121,264 @@ class HomeView extends StatefulWidget {
               onTap: () {},
             )
           ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        controller: ScrollController(),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                height: 160.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      16.0,
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black26,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 20.0,
+                      top: 0.0,
+                      bottom: 0.0,
+                      child: SizedBox(
+                        width: 100.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Get special discount",
+                              style: GoogleFonts.oswald(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Up to 50%",
+                              style: GoogleFonts.oswald(
+                                fontSize: 22.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 6.0,
+                            ),
+                            Container(
+                              height: 28.0,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  shape: const StadiumBorder(),
+                                ),
+                                onPressed: () {},
+                                child: const Text(
+                                  "Claim Voucher",
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 9,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6.0,
+                  horizontal: 12.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: null,
+                        decoration: InputDecoration.collapsed(
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          hintText: "What are you craving?",
+                          hintStyle: TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                          hoverColor: Colors.transparent,
+                        ),
+                        onFieldSubmitted: (value) {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              QCategoryPicker(
+                items: controller.categories,
+                onChanged: (index, label, value, item) {},
+                itemBuilder: (item, selected, action) {
+                  return InkWell(
+                    onTap: () => action(),
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        right: 12.0,
+                      ),
+                      height: 86,
+                      width: 86,
+                      decoration: BoxDecoration(
+                        color: selected ? Colors.orange : Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            16.0,
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            item["icon"],
+                            size: 24.0,
+                            color: selected ? Colors.white : null,
+                          ),
+                          Text(
+                            item["label"],
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              GridView.builder(
+                padding: EdgeInsets.zero,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1.0 / 1.34,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                ),
+                itemCount: controller.products.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var item = controller.products[index];
+                  return Container(
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Image.network(
+                            item["photo"],
+                            width: 200.0,
+                            height: 200.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          item["product_name"],
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 3.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            const Text(
+                              "30 Min",
+                              style:
+                                  TextStyle(fontSize: 10.0, color: Colors.grey),
+                            ),
+                            const SizedBox(
+                              width: 3.0,
+                            ),
+                            Text(
+                              "|",
+                              style:
+                                  TextStyle(fontSize: 10.0, color: Colors.grey),
+                            ),
+                            const SizedBox(
+                              width: 3.0,
+                            ),
+                            const Text(
+                              "120 Sell",
+                              style:
+                                  TextStyle(fontSize: 10.0, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3.0,
+                        ),
+                        Text(
+                          "\$${item["price"]}",
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
